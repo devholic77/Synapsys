785516868d88d4533ca23d2f84571026d8c9f60c
dhuck <dhuckilkim@gmail.com>
2015-03-05 15:36:53 +0900

    [Android] MainActivity에서 터치 이벤트 처리로 변경

--- Android_Part/Synapsys/src/org/gbssm/synapsys/MainActivity.java
+++ Android_Part/Synapsys/src/org/gbssm/synapsys/MainActivity.java
@@ -1,52 +1,68 @@
 package org.gbssm.synapsys;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
 
 /**
- *  애플리케이션을 시작하는 Main Activity. 
+ * 애플리케이션을 시작하는 Main Activity.
  * 
  * @author Yeonho.Kim
  * @since 2015.03.05
- *
+ * 
  */
 public class MainActivity extends Activity {
+
+	private WindowsTouchListener mTouchListener;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(new StreamingView(this));
+		mTouchListener = new WindowsTouchListener(this);
 	}
-	
+
 	@Override
 	protected void onStart() {
 		// TODO Auto-generated method stub
 		super.onStart();
 	}
-	
+
 	@Override
 	protected void onResume() {
 		// TODO Auto-generated method stub
 		super.onResume();
 	}
-	
+
 	@Override
 	protected void onPause() {
 		// TODO Auto-generated method stub
 		super.onPause();
 	}
-	
+
 	@Override
 	protected void onStop() {
-		// TODO Auto-generated method stub
+		// TODO Auto-g
+	
 		super.onStop();
 	}
-	
+
 	@Override
 	protected void onDestroy() {
 		// TODO Auto-generated method stub
 		super.onDestroy();
 	}
-	
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (mTouchListener != null)
+			return mTouchListener.onTouch(event);
+
+		return false;
+	}
+
 }
--- Android_Part/Synapsys/src/org/gbssm/synapsys/WindowsTouchListener.java
+++ Android_Part/Synapsys/src/org/gbssm/synapsys/WindowsTouchListener.java
@@ -1,7 +1,9 @@
 package org.gbssm.synapsys;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.GestureDetector;
+import android.view.GestureDetector.OnDoubleTapListener;
 import android.view.GestureDetector.OnGestureListener;
 import android.view.MotionEvent;
 import android.view.View;
@@ -13,33 +15,33 @@ import android.widget.Toast;
  * 
  * @author dhuckil.kim
  * @since 2015.03.05
- *
+ * 
  */
-public class WindowsTouchListener implements OnTouchListener, OnGestureListener {
+public class WindowsTouchListener implements OnGestureListener,
+		OnDoubleTapListener {
 
 	private final Context mContextF;
-	
-	private GestureDetector gd;
-	
+
+	private GestureDetector mgestureDetector;
+
 	public WindowsTouchListener(Context context) {
 		mContextF = context;
-		
+
 		// TODO Auto-generated constructor stub
-		gd = new GestureDetector(mContextF, this);
+		mgestureDetector = new GestureDetector(mContextF, this);
+		mgestureDetector.setIsLongpressEnabled(true);
+		mgestureDetector.setOnDoubleTapListener(this);
 	}
-	
-	@Override
-	public boolean onTouch(View v, MotionEvent event) {
-		// TODO Auto-generated method stub
-		
-		
-		return gd.onTouchEvent(event);
+
+	public boolean onTouch(MotionEvent e) {
+
+		return mgestureDetector.onTouchEvent(e);
 	}
 
 	@Override
 	public boolean onDown(MotionEvent e) {
 		// TODO Auto-generated method stub
-		
+		Log.d("Touch listener", "On Down");
 		return false;
 	}
 
@@ -47,35 +49,59 @@ public class WindowsTouchListener implements OnTouchListener, OnGestureListener 
 	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
 			float velocityY) {
 		// TODO Auto-generated method stub
-		
-		Toast.makeText(mContextF, "onFling!! : " + e1.getX() +", " + e1.getY(), Toast.LENGTH_SHORT).show();
+		Log.d("Touch listener", "On Fling");
 		return false;
 	}
 
 	@Override
 	public void onLongPress(MotionEvent e) {
 		// TODO Auto-generated method stub
-
-		Toast.makeText(mContextF, "onLongPress!! : " + e.getX() +", " + e.getY(), Toast.LENGTH_SHORT).show();
+		Log.d("Touch listener", "On Longpress");
+		// Toast.makeText(mContextF, "onLongPress!! : " + e.getX() +", " +
+		// e.getY(), Toast.LENGTH_SHORT).show();
 	}
 
 	@Override
-	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+			float distanceY) {
 		// TODO Auto-generated method stub
+		Log.d("Touch listener", "On Scroll");
 		return false;
 	}
 
 	@Override
 	public void onShowPress(MotionEvent e) {
 		// TODO Auto-generated method stub
-		
+		Log.d("Touch listener", "On showPress");
+
 	}
 
 	@Override
 	public boolean onSingleTapUp(MotionEvent e) {
 		// TODO Auto-generated method stub
+		Log.d("Touch listener", "On singletapup");
 		return false;
 	}
 
-	
+	@Override
+	public boolean onSingleTapConfirmed(MotionEvent e) {
+		// TODO Auto-generated method stub
+		Log.d("Touch listener", "On singletapconfirmed");
+		return false;
+	}
+
+	@Override
+	public boolean onDoubleTap(MotionEvent e) {
+		// TODO Auto-generated method stub
+		Log.d("Touch listener", "On doubletap");
+		return false;
+	}
+
+	@Override
+	public boolean onDoubleTapEvent(MotionEvent e) {
+		// TODO Auto-generated method stub
+		Log.d("Touch listener", "On doubletapevent");
+		return false;
+	}
+
 }
