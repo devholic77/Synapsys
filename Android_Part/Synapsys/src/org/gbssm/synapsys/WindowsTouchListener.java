8b64f03ff702e865d3db24894b6e5f830b58aaa4
dhuck <dhuckilkim@gmail.com>
2015-03-06 09:02:11 +0900

    [Android] Touch Event 구현 1차 완료.

--- Android_Part/Synapsys/src/org/gbssm/synapsys/WindowsTouchListener.java
+++ Android_Part/Synapsys/src/org/gbssm/synapsys/WindowsTouchListener.java
@@ -15,8 +15,7 @@ import android.view.MotionEvent;
  * 
  */
 
-public class WindowsTouchListener implements OnGestureListener,
-		OnDoubleTapListener {
+public class WindowsTouchListener implements OnGestureListener,OnDoubleTapListener {
 
 	final static int LEFT_CLICK = 1;
 	final static int LEFT_DRAG = 2;
@@ -24,8 +23,13 @@ public class WindowsTouchListener implements OnGestureListener,
 	final static int RIGHT_CLICK = 4;
 	final static int SCROLL_UP_DOWN = 5;
 	final static int SCROLL_LEFT_RIGHT = 6;
+	final static int EVENT_END = 7;
 	int Doubletap_flag = 0;
-	int Drag_flag = 0;
+	int MultiTouch_flag = 0;
+	int Scroll_flag = 0;
+	int act = 0;
+
+	MotionEvent temp_e;
 
 	private final Context mContextF;
 	private GestureDetector mGestureDetector;
@@ -38,6 +42,22 @@ public class WindowsTouchListener implements OnGestureListener,
 	}
 
 	public boolean onTouchEvent(MotionEvent e) {
+		
+		temp_e = e;
+		act = temp_e.getAction();
+		
+		if (act == MotionEvent.ACTION_POINTER_2_DOWN) {		// 멀티 터치가 되었을 때 
+			
+			MultiTouch_flag = 1;
+		}
+		else if(Scroll_flag == 1 && act == MotionEvent.ACTION_UP){	// 스크롤이 되고 이벤트가 끝났을 때 		
+			
+			Scroll_flag = 0;
+			Log.d("Touch listener","On Scroll End" + " AXIS_X :"	+ e.getAxisValue(e.AXIS_X) + " AXIS_Y :"+ e.getAxisValue(e.AXIS_Y));
+			MouseEvent(EVENT_END, e.getAxisValue(e.AXIS_X),e.getAxisValue(e.AXIS_Y));		
+			MultiTouch_flag = 0;
+		}
+	
 		return mGestureDetector.onTouchEvent(e);
 	}
 
@@ -45,7 +65,6 @@ public class WindowsTouchListener implements OnGestureListener,
 	public boolean onDown(MotionEvent e) {
 		// TODO Auto-generated method stub
 		Log.d("Touch listener", "On Down");
-		Drag_flag = 1;		//드래그 상태 확인
 		return false;
 	}
 
@@ -53,37 +72,25 @@ public class WindowsTouchListener implements OnGestureListener,
 	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
 			float velocityY) {
 		// TODO Auto-generated method stub
-		Log.d("Touch listener", "On Fling");
+
+		MultiTouch_flag = 0;
 		return false;
 	}
 
 	@Override
-	// 마우스 우클릭 
-	public void onLongPress(MotionEvent e) {
+	// 마우스 좌 클릭 드래그
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,float distanceY) {
 		// TODO Auto-generated method stub
-		Log.d("Touch listener",
-				"On Long press" + "AXIS_X :" + e.getAxisValue(e.AXIS_X)
-						+ " AXIS_Y :" + e.getAxisValue(e.AXIS_Y));
-		// Toast.makeText(mContextF, "onLongPress!! : " + e.getX() +", " +
-		// e.getY(), Toast.LENGTH_SHORT).show();
-		MouseEvent(RIGHT_CLICK, e.getAxisValue(e.AXIS_X),
-				e.getAxisValue(e.AXIS_Y));
-	}
-
-	
-	@Override
-	//마우스 좌 클릭 드래그 
-	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
-			float distanceY) {
-		// TODO Auto-generated method stub
-		if(Drag_flag == 1)
-		{
-			Log.d("Touch listener", "On Scroll"+ " AXIS_X :" + e2.getAxisValue(e2.AXIS_X)
-					+ " AXIS_Y :" + e2.getAxisValue(e2.AXIS_Y));
-			MouseEvent(LEFT_DRAG, e2.getAxisValue(e2.AXIS_X),
-					e2.getAxisValue(e2.AXIS_Y));
-		}
+		Scroll_flag = 1;
 		
+		if (MultiTouch_flag == 1) {
+			Log.d("Touch listener","On Double Scroll" + " AXIS_X :"+ e2.getAxisValue(e2.AXIS_X) + " AXIS_Y :"+ e2.getAxisValue(e2.AXIS_Y));
+			MouseEvent(SCROLL_UP_DOWN, e2.getAxisValue(e2.AXIS_X),e2.getAxisValue(e2.AXIS_Y));
+		} else if (MultiTouch_flag == 0) {
+			Log.d("Touch listener","On Drag" + " AXIS_X :" + e2.getAxisValue(e2.AXIS_X)+ " AXIS_Y :" + e2.getAxisValue(e2.AXIS_Y));
+			MouseEvent(LEFT_DRAG, e2.getAxisValue(e2.AXIS_X),e2.getAxisValue(e2.AXIS_Y));
+		}
+
 		return false;
 	}
 
@@ -103,15 +110,11 @@ public class WindowsTouchListener implements OnGestureListener,
 	}
 
 	@Override
-	// 마우스 좌 클릭 
+	// 마우스 좌 클릭
 	public boolean onSingleTapConfirmed(MotionEvent e) {
 		// TODO Auto-generated method stub
-		Log.d("Touch listener",
-				"On single Tap confirmed" + "AXIS_X :"
-						+ e.getAxisValue(e.AXIS_X) + " AXIS_Y :"
-						+ e.getAxisValue(e.AXIS_Y));
-		MouseEvent(LEFT_CLICK, e.getAxisValue(e.AXIS_X),
-				e.getAxisValue(e.AXIS_Y));
+		Log.d("Touch listener","On single Tap confirmed" + "AXIS_X :"+ e.getAxisValue(e.AXIS_X) + " AXIS_Y :"+ e.getAxisValue(e.AXIS_Y));
+		MouseEvent(LEFT_CLICK, e.getAxisValue(e.AXIS_X),	e.getAxisValue(e.AXIS_Y));
 		return false;
 	}
 
@@ -125,54 +128,63 @@ public class WindowsTouchListener implements OnGestureListener,
 	}
 
 	@Override
-	// 마우스 좌 더블 클릭 
+	// 마우스 좌 더블 클릭
 	public boolean onDoubleTapEvent(MotionEvent e) {
 		// TODO Auto-generated method stub
 		if (Doubletap_flag == 1) {
-			Log.d("Touch listener",
-					"On double Tap event" + " AXIS_X :"
-							+ e.getAxisValue(e.AXIS_X) + " AXIS_Y :"
-							+ e.getAxisValue(e.AXIS_Y));
-			MouseEvent(LEFT_DOUBLE_CLICK, e.getAxisValue(e.AXIS_X),e.getAxisValue(e.AXIS_Y));
+			Log.d("Touch listener","On double Tap event" + " AXIS_X :"+ e.getAxisValue(e.AXIS_X) + " AXIS_Y :"+ e.getAxisValue(e.AXIS_Y));
+			MouseEvent(LEFT_DOUBLE_CLICK, e.getAxisValue(e.AXIS_X),	e.getAxisValue(e.AXIS_Y));
 			Doubletap_flag = 0;
 		}
 
 		return false;
 	}
-	// 테스트
+
+	@Override
+	// 마우스 우클릭
+	public void onLongPress(MotionEvent e) {
+		// TODO Auto-generated method stub
+		Log.d("Touch listener","On Long press" + "AXIS_X :" + e.getAxisValue(e.AXIS_X)+ " AXIS_Y :" + e.getAxisValue(e.AXIS_Y));
+		MouseEvent(RIGHT_CLICK, e.getAxisValue(e.AXIS_X),e.getAxisValue(e.AXIS_Y));
+	}
+
 	/*
 	 * MouseEvent 처리 메소드
 	 */
 	public void MouseEvent(int event_id, float point_x, float point_y) {
 		switch (event_id) {
 		case LEFT_CLICK:
-			// Log.d("Touch listener", "Send to event : left_click");
+			Log.d("Touch Event Call","On Left Click");						
 			break;
 
 		case LEFT_DRAG:
-
+			Log.d("Touch Event Call","On Left Drag");	
 			break;
 
 		case LEFT_DOUBLE_CLICK:
-			// Log.d("Touch listener", "Send to event : left_double_click");
+			Log.d("Touch Event Call","On Left Double Click");	
 			break;
 
 		case RIGHT_CLICK:
-			// Log.d("Touch listener", "Send to event : right_click");
+			Log.d("Touch Event Call","On Right Click");	
 			break;
 
 		case SCROLL_LEFT_RIGHT:
-
+			Log.d("Touch Event Call","On Scroll Left Right");	
 			break;
 
 		case SCROLL_UP_DOWN:
-
+			Log.d("Touch Event Call","On Scroll Up Down");	
 			break;
 
+		case EVENT_END:
+			Log.d("Touch Event Call","On Event End");	
+			break;
 		default:
 
 			break;
 		}
+	
 
 	}
 
