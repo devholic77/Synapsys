9fedb6cb8bb11d6ae079e8798c5cc41c1d22a998
jominhwan <minhwan000@gmail.com>
2015-04-15 00:49:08 +0900

    re123

--- Synapsys/MainWindow.xaml.cs
+++ Synapsys/MainWindow.xaml.cs
@@ -194,7 +194,7 @@ namespace Synapsys
 
             // public  String Check_Deivce_Msg { get; set; } //Add, Remove
             //public  int Check_Device_Flag { get; set; } // 1, 2, 3
-
+            //zz
             //(Check_Deivce_Msg 에 따라서)
             // 1 - First Monitor가 추가 또는 제거됨 
             // 2 - Second Monitor가 추가 또는 제거됨 
