1a64a47b9cf8370031de64fd255fa5740526d9da
Daechan Jang <jang9129@skku.edu>
2015-04-26 17:46:52 +0900

    changed

--- Synapsys/CaptureScreen.cs
+++ Synapsys/CaptureScreen.cs
@@ -21,7 +21,7 @@ namespace Synapsys
 		private static int MAX_MONITOR = 3;
 		private static Screen[] scrs;
 
-		public static volatile int currFPS = 0;
+		public static int currFPS = 0;
 
 		// 싱글톤
 		public static CaptureScreen getInstance()
@@ -150,7 +150,7 @@ namespace Synapsys
 
 		private static void capture()
 		{
-			//currFPS++;
+			currFPS++;
 			try
 			{
 				int SUBSCREEN = (Screen.AllScreens.Length - 1);
@@ -164,39 +164,15 @@ namespace Synapsys
 					Rectangle rect = new Rectangle(0, 0, MONITOR_WIDTH, MONITOR_HEIGHT);
 					if(SUBSCREEN >= 1)
 					{
-
-						using (Bitmap firstHalf1 = bitmap.Clone(rect, bitmap.PixelFormat))
+						using (Bitmap firstHalf = bitmap.Clone(rect, bitmap.PixelFormat))
 						{
-							using (Graphics g = Graphics.FromImage(bitmap))
+							using (MemoryStream stream = new MemoryStream())
 							{
-								g.CopyFromScreen(new Point(-MONITOR_WIDTH * SUBSCREEN, 0), Point.Empty, new Size(MONITOR_WIDTH * SUBSCREEN, MONITOR_HEIGHT));
+								firstHalf.Save(stream, ImageFormat.Jpeg);
+								MainWindow.socketIMG1.SendScreen(stream.ToArray());
+								stream.Close();
 							}
-
-							using (Bitmap firstHalf2 = bitmap.Clone(rect, bitmap.PixelFormat))
-							{
-								//if (!compare(firstHalf1, firstHalf2))
-								
-									using (MemoryStream stream = new MemoryStream())
-									{
-										currFPS++;
-										firstHalf2.Save(stream, ImageFormat.Jpeg);
-										MainWindow.socketIMG1.SendScreen(stream.ToArray());
-										stream.Close();
-									}
-							}
-
 						}
-
-
-						//using (Bitmap firstHalf = bitmap.Clone(rect, bitmap.PixelFormat))
-						//{
-						//	using (MemoryStream stream = new MemoryStream())
-						//	{
-						//		firstHalf.Save(stream, ImageFormat.Jpeg);
-						//		MainWindow.socketIMG1.SendScreen(stream.ToArray());
-						//		stream.Close();
-						//	}
-						//}
 					}
 
 					// #2					
