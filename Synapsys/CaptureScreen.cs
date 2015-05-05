843a77ab11edec5accf3c632dcf9794cb05b8ceb
Daechan Jang <jang9129@skku.edu>
2015-05-05 16:15:34 +0900

    Mouse draw disable

--- Synapsys/CaptureScreen.cs
+++ Synapsys/CaptureScreen.cs
@@ -173,17 +173,17 @@ namespace Synapsys
 					{
 						g.CopyFromScreen(new Point(MainWindow.MONITOR_FIRST_WIDTH, 0), Point.Empty, bitmap.Size);
 
-						CURSORINFO pci;
-						pci.cbSize = System.Runtime.InteropServices.Marshal.SizeOf(typeof(CURSORINFO));
+						//CURSORINFO pci;
+						//pci.cbSize = System.Runtime.InteropServices.Marshal.SizeOf(typeof(CURSORINFO));
 
-						if (GetCursorInfo(out pci))
-						{
-							if (pci.flags == CURSOR_SHOWING && pci.ptScreenPos.x >= MainWindow.MONITOR_FIRST_WIDTH)
-							{
-								DrawIcon(g.GetHdc(), pci.ptScreenPos.x - MainWindow.MONITOR_FIRST_WIDTH, pci.ptScreenPos.y, pci.hCursor);
-								g.ReleaseHdc();
-							}
-						}
+						//if (GetCursorInfo(out pci))
+						//{
+						//	if (pci.flags == CURSOR_SHOWING && pci.ptScreenPos.x >= MainWindow.MONITOR_FIRST_WIDTH)
+						//	{
+						//		DrawIcon(g.GetHdc(), pci.ptScreenPos.x - MainWindow.MONITOR_FIRST_WIDTH, pci.ptScreenPos.y, pci.hCursor);
+						//		g.ReleaseHdc();
+						//	}
+						//}
 					}
 					// #1
 					Rectangle rect = new Rectangle(0, 0, MONITOR_WIDTH, MONITOR_HEIGHT);
@@ -265,28 +265,28 @@ namespace Synapsys
 			return equals;
 		}
 
-		[StructLayout(LayoutKind.Sequential)]
-		struct CURSORINFO
-		{
-			public Int32 cbSize;
-			public Int32 flags;
-			public IntPtr hCursor;
-			public POINTAPI ptScreenPos;
-		}
+		//[StructLayout(LayoutKind.Sequential)]
+		//struct CURSORINFO
+		//{
+		//	public Int32 cbSize;
+		//	public Int32 flags;
+		//	public IntPtr hCursor;
+		//	public POINTAPI ptScreenPos;
+		//}
 
-		[StructLayout(LayoutKind.Sequential)]
-		struct POINTAPI
-		{
-			public int x;
-			public int y;
-		}
+		//[StructLayout(LayoutKind.Sequential)]
+		//struct POINTAPI
+		//{
+		//	public int x;
+		//	public int y;
+		//}
 
-		[DllImport("user32.dll")]
-		static extern bool GetCursorInfo(out CURSORINFO pci);
+		//[DllImport("user32.dll")]
+		//static extern bool GetCursorInfo(out CURSORINFO pci);
 
-		[DllImport("user32.dll")]
-		static extern bool DrawIcon(IntPtr hDC, int X, int Y, IntPtr hIcon);
+		//[DllImport("user32.dll")]
+		//static extern bool DrawIcon(IntPtr hDC, int X, int Y, IntPtr hIcon);
 
-		const Int32 CURSOR_SHOWING = 0x00000001;
+		//const Int32 CURSOR_SHOWING = 0x00000001;
 	}
 }
